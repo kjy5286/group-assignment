8ab2cbe96fc86c6859bcf48da9736ae81ef188ac
Youngwuk Kwon <kwon_young02@naver.com>
2021-06-04 17:16:05 +0900

    카지노업데이트

--- assignment/group_assignment/group_assignment.cpp
+++ assignment/group_assignment/group_assignment.cpp
@@ -12,15 +12,15 @@
 #define bba 2
 
 //카지노 게임 전역변수 선언
-SceneID pubMain,rspBackground,oddBackground;
-ObjectID menu1,menu2,RSP,odd,rspBut,oddBut,casino,enterLobby;
-ObjectID rspMachine, exitButton, introduction,randEnemy,restart;
-ObjectID rspButton[3],percentT[3], percentF[3],handMe[3],resultT[4],resultF[4], handEnermy[3];
+SceneID pubMain, rspBackground, oddBackground;
+ObjectID menu1, menu2, RSP, odd, rspBut, oddBut, casino, enterLobby;
+ObjectID rspMachine, exitButton, introduction, randEnemy, restart;
+ObjectID rspButton[3], percentT[3], percentF[3], handMe[3], resultT[4], resultF[4], handEnermy[3];
 ObjectID hol, chak, back, front[11], percentTT[6], percentFF[6], flip, regame, exitBut;
-SoundID CasinoBGM,buttonSound,win,draw,lose;
+SoundID CasinoBGM, buttonSound, win, draw, lose;
 int magnification[10] = { 1,2,3,1,1,2,2,1,5,20 };
 int magnification2[10] = { 1,2,1,5,1,2,3,1,3,2 };
-int battingPercent = 10,enermy;
+int battingPercent = 10, enermy;
 double cost;
 int jay;
 
@@ -192,7 +192,7 @@ void endOdd() {
 //홀짝 숫자뽑기 및 게임 진행 함수(결과, 종료 등)
 void randomnum(int j) {
 	int randomTemp = rand() % 10 + 1;
-	for (int i = 1; i < 11; i ++) {
+	for (int i = 1; i < 11; i++) {
 		if (randomTemp == i) {
 			hideObject(back);
 			showObject(front[i]);
@@ -245,8 +245,8 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 				hideObject(percentT[i + 1]);
 				hideObject(percentT[i + 2]);
 				showObject(percentT[i]);
-				showObject(percentF[i+1]);
-				showObject(percentF[i+2]);
+				showObject(percentF[i + 1]);
+				showObject(percentF[i + 2]);
 				battingPercent = 10;
 			}
 			if (i == 1) {
@@ -276,7 +276,7 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 			enermy = rand() % 3;
 			if (j == 0) {
 				showObject(handMe[j]);
-				hideObject(handMe[j + 1]);	
+				hideObject(handMe[j + 1]);
 				hideObject(handMe[j + 2]);
 				showObject(handEnermy[enermy]);
 				int result = RspGame(j, enermy);
@@ -314,8 +314,9 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 		}
 	}
 
-/* 홀짝 관련 코드*/ 
+	/* 홀짝 관련 코드*/
 	if (object == hol) {
+		playSound(buttonSound);
 		showObject(flip);
 		for (int i = 1; i < 3; i++) {
 			hideObject(percentFF[0]);
@@ -330,6 +331,7 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 		jay = 1;
 	}
 	if (object == chak) {
+		playSound(buttonSound);
 		showObject(flip);
 		for (int i = 4; i < 6; i++) {
 			hideObject(percentFF[3]);
@@ -381,6 +383,7 @@ void mouseCallback(ObjectID object, int x, int y, MouseAction action) {
 		hideObject(flip);
 	}
 	if (object == regame) {
+		playSound(buttonSound);
 		showObject(hol);
 		showObject(chak);
 		for (int i = 1; i < 11; i++) {
@@ -421,98 +424,98 @@ ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown
 
 //카지노 변수 할당
 void MakeCasino() {
-	pubMain = createScene("", "pub.png");
-	rspBackground = createScene("", "rspBackground.png");
-	oddBackground = createScene("", "oddBackground.png");
+	pubMain = createScene("", "source/picture/casino/pub.png");
+	rspBackground = createScene("", "source/picture/casino/rspBackground.png");
+	oddBackground = createScene("", "source/picture/casino/oddBackground.png");
 
-	casino = createObject("casino.png", pubMain, 20, 35, true);
-	menu1 = createObject("gamemachine.png", pubMain, 298, 150, true);
+	casino = createObject("source/picture/casino/casino.png", pubMain, 20, 35, true);
+	menu1 = createObject("source/picture/casino/gamemachine.png", pubMain, 298, 150, true);
 	scaleObject(menu1, 0.3f);
-	menu2 = createObject("gamemachine.png", pubMain, 700, 150, true);
+	menu2 = createObject("source/picture/casino/gamemachine.png", pubMain, 700, 150, true);
 	scaleObject(menu2, 0.3f);
-	RSP = createObject("RSP.png", pubMain, 360, 261, true);
+	RSP = createObject("source/picture/casino/RSP.png", pubMain, 360, 261, true);
 	scaleObject(RSP, 0.25f);
-	odd = createObject("odd.png", pubMain, 775, 261, true);
+	odd = createObject("source/picture/casino/odd.png", pubMain, 775, 261, true);
 	scaleObject(odd, 0.25f);
-	rspBut = createObject("rspButton.png", pubMain, 298, 65, true);
+	rspBut = createObject("source/picture/casino/rspButton.png", pubMain, 298, 65, true);
 	scaleObject(rspBut, 0.8f);
-	oddBut = createObject("oddButton.png", pubMain, 704, 65, true);
+	oddBut = createObject("source/picture/casino/oddButton.png", pubMain, 704, 65, true);
 	scaleObject(oddBut, 0.8f);
 
-	rspMachine = createObject("rspMachine.png", rspBackground, 70, 80, true);
+	rspMachine = createObject("source/picture/casino/rspMachine.png", rspBackground, 70, 80, true);
 	scaleObject(rspMachine, 1.3f);
-	rspButton[0] = createObject("rockButton.png", rspBackground, 200, 110, true);
-	rspButton[1] = createObject("jjiButton.png", rspBackground, 360, 110, true);
-	rspButton[2] = createObject("bbaButton.png", rspBackground, 520, 110, true);
+	rspButton[0] = createObject("source/picture/casino/mookButton.png", rspBackground, 200, 110, true);
+	rspButton[1] = createObject("source/picture/casino/jjiButton.png", rspBackground, 360, 110, true);
+	rspButton[2] = createObject("source/picture/casino/bbaButton.png", rspBackground, 520, 110, true);
 	for (int i = 0; i < 3; i++) {
 		scaleObject(rspButton[i], 0.5f);
 	}
-	enterLobby = createObject("exit.png", pubMain, 5, 5, true);
+	enterLobby = createObject("source/picture/casino/exit.png", pubMain, 5, 5, true);
 	scaleObject(enterLobby, 0.06f);
-	exitButton = createObject("exit.png", rspBackground, 5, 5, true);
+	exitButton = createObject("source/picture/casino/exit.png", rspBackground, 5, 5, true);
 	scaleObject(exitButton, 0.06f);
-	introduction = createObject("introduction.png", rspBackground, 800, 370, true);
+	introduction = createObject("source/picture/casino/introduction.png", rspBackground, 800, 370, true);
 	scaleObject(introduction, 0.5f);
 
-	percentT[0] = createObject("10%T.png", rspBackground, 800, 150, true);
-	percentT[1] = createObject("50%T.png", rspBackground, 950, 150, false);
-	percentT[2] = createObject("100%T.png", rspBackground, 1100, 150, false);
+	percentT[0] = createObject("source/picture/casino/10%T.png", rspBackground, 800, 150, true);
+	percentT[1] = createObject("source/picture/casino/50%T.png", rspBackground, 950, 150, false);
+	percentT[2] = createObject("source/picture/casino/100%T.png", rspBackground, 1100, 150, false);
 	for (int i = 0; i < 3; i++) {
 		scaleObject(percentT[i], 0.5f);
 	}
 
-	percentF[0] = createObject("10%F.png", rspBackground, 800, 150, false);
-	percentF[1] = createObject("50%F.png", rspBackground, 945, 150, true);
-	percentF[2] = createObject("100%F.png", rspBackground, 1100, 150, true);
+	percentF[0] = createObject("source/picture/casino/10%F.png", rspBackground, 800, 150, false);
+	percentF[1] = createObject("source/picture/casino/50%F.png", rspBackground, 945, 150, true);
+	percentF[2] = createObject("source/picture/casino/100%F.png", rspBackground, 1100, 150, true);
 	for (int i = 0; i < 3; i++) {
 		scaleObject(percentF[i], 0.5f);
 	}
 
-	handEnermy[mook] = createObject("mook3.png", rspBackground, 230, 350, false);
-	handEnermy[jji] = createObject("jji3.png", rspBackground, 230, 350, false);
-	handEnermy[bba] = createObject("bba3.png", rspBackground, 230, 340, false);
+	handEnermy[mook] = createObject("source/picture/casino/mook3.png", rspBackground, 230, 350, false);
+	handEnermy[jji] = createObject("source/picture/casino/jji3.png", rspBackground, 230, 350, false);
+	handEnermy[bba] = createObject("source/picture/casino/bba3.png", rspBackground, 230, 340, false);
 
-	handMe[mook] = createObject("mook2.png", rspBackground, 560, 380, false);
-	handMe[jji] = createObject("jji2.png", rspBackground, 550, 380, false);
-	handMe[bba] = createObject("bba2.png", rspBackground, 560, 380, false);
+	handMe[mook] = createObject("source/picture/casino/mook2.png", rspBackground, 560, 380, false);
+	handMe[jji] = createObject("source/picture/casino/jji2.png", rspBackground, 550, 380, false);
+	handMe[bba] = createObject("source/picture/casino/bba2.png", rspBackground, 560, 380, false);
 	for (int i = 0; i < 3; i++) {
 		scaleObject(handMe[i], 0.5f);
 	}
 
-	resultT[0] = createObject("winT.png", rspBackground, 160, 530, false);
-	resultT[1] = createObject("winT.png", rspBackground, 420, 530, false);
-	resultT[2] = createObject("drawT.png", rspBackground, 160, 270, false);
-	resultT[3] = createObject("loseT.png", rspBackground, 420, 270, false);
+	resultT[0] = createObject("source/picture/casino/winT.png", rspBackground, 160, 530, false);
+	resultT[1] = createObject("source/picture/casino/winT.png", rspBackground, 420, 530, false);
+	resultT[2] = createObject("source/picture/casino/drawT.png", rspBackground, 160, 270, false);
+	resultT[3] = createObject("source/picture/casino/loseT.png", rspBackground, 420, 270, false);
 	for (int i = 0; i < 4; i++) {
 		scaleObject(resultT[i], 0.25f);
 	}
 
-	resultF[0] = createObject("winF.png", rspBackground, 160, 530, true);
-	resultF[1] = createObject("winF.png", rspBackground, 420, 530, true);
-	resultF[2] = createObject("drawF.png", rspBackground, 160, 270, true);
-	resultF[3] = createObject("loseF.png", rspBackground, 420, 270, true);
+	resultF[0] = createObject("source/picture/casino/winF.png", rspBackground, 160, 530, true);
+	resultF[1] = createObject("source/picture/casino/winF.png", rspBackground, 420, 530, true);
+	resultF[2] = createObject("source/picture/casino/drawF.png", rspBackground, 160, 270, true);
+	resultF[3] = createObject("source/picture/casino/loseF.png", rspBackground, 420, 270, true);
 	for (int i = 0; i < 4; i++) {
 		scaleObject(resultF[i], 0.25f);
 	}
 
-	restart = createObject("restart.png", rspBackground, 115, 110, false);
+	restart = createObject("source/picture/casino/restart.png", rspBackground, 115, 110, false);
 
-	CasinoBGM = createSound2("CasinoBGM.mp3", true);
-	buttonSound = createSound2("buttonSound.mp3", false);
-	win = createSound2("win.mp3", false);
-	draw = createSound2("draw.mp3", false);
-	lose = createSound2("lose.mp3", false);
+	CasinoBGM = createSound2("source/sound/CasinoBGM.mp3", true);
+	buttonSound = createSound2("source/sound/buttonSound.mp3", false);
+	win = createSound2("source/sound/win.mp3", false);
+	draw = createSound2("source/sound/draw.mp3", false);
+	lose = createSound2("source/sound/lose.mp3", false);
 
 	// 홀짝
-	hol = createObject("hol.png", oddBackground, 150, 360, true);
+	hol = createObject("source/picture/casino/hol.png", oddBackground, 150, 360, true);
 	scaleObject(hol, 2.3f);
-	chak = createObject("chak.png", oddBackground, 970, 360, true);
+	chak = createObject("source/picture/casino/chak.png", oddBackground, 970, 360, true);
 	scaleObject(chak, 2.3f);
-	back = createObject("back.png", oddBackground, 333, 70, true);
-	flip = createObject("flip.png", oddBackground, 450, 70, false);
+	back = createObject("source/picture/casino/back.png", oddBackground, 333, 70, true);
+	flip = createObject("source/picture/casino/flip.png", oddBackground, 450, 70, false);
 	scaleObject(flip, 1.2f);
-	regame = createObject("restart.png", oddBackground, 330, 10, false);
-	exitBut = createObject("exit.png", oddBackground, 5, 5, true);
+	regame = createObject("source/picture/casino/restart.png", oddBackground, 330, 10, false);
+	exitBut = createObject("source/picture/casino/exit.png", oddBackground, 5, 5, true);
 	scaleObject(exitBut, 0.06f);
 
 	char frontnum[50];
@@ -522,30 +525,30 @@ void MakeCasino() {
 		scaleObject(front[i], 1.02f);
 	}
 
-	percentTT[0] = createObject("10%T.png", oddBackground, 15, 200, false);
-	percentTT[1] = createObject("50%T.png", oddBackground, 160, 200, false);
-	percentTT[2] = createObject("100%T.png", oddBackground, 315, 200, false);
+	percentTT[0] = createObject("source/picture/casino/10%T.png", oddBackground, 15, 200, false);
+	percentTT[1] = createObject("source/picture/casino/50%T.png", oddBackground, 160, 200, false);
+	percentTT[2] = createObject("source/picture/casino/100%T.png", oddBackground, 315, 200, false);
 	for (int i = 0; i < 3; i++) {
 		scaleObject(percentTT[i], 0.5f);
 	}
 
-	percentFF[0] = createObject("10%F.png", oddBackground, 15, 200, false);
-	percentFF[1] = createObject("50%F.png", oddBackground, 160, 200, false);
-	percentFF[2] = createObject("100%F.png", oddBackground, 315, 200, false);
+	percentFF[0] = createObject("source/picture/casino/10%F.png", oddBackground, 15, 200, false);
+	percentFF[1] = createObject("source/picture/casino/50%F.png", oddBackground, 160, 200, false);
+	percentFF[2] = createObject("source/picture/casino/100%F.png", oddBackground, 315, 200, false);
 	for (int i = 0; i < 3; i++) {
 		scaleObject(percentFF[i], 0.5f);
 	}
 
-	percentTT[3] = createObject("10%T.png", oddBackground, 837, 200, false);
-	percentTT[4] = createObject("50%T.png", oddBackground, 982, 200, false);
-	percentTT[5] = createObject("100%T.png", oddBackground, 1137, 200, false);
+	percentTT[3] = createObject("source/picture/casino/10%T.png", oddBackground, 837, 200, false);
+	percentTT[4] = createObject("source/picture/casino/50%T.png", oddBackground, 982, 200, false);
+	percentTT[5] = createObject("source/picture/casino/100%T.png", oddBackground, 1137, 200, false);
 	for (int i = 3; i < 6; i++) {
 		scaleObject(percentTT[i], 0.5f);
 	}
 
-	percentFF[3] = createObject("10%F.png", oddBackground, 837, 200, false);
-	percentFF[4] = createObject("50%F.png", oddBackground, 982, 200, false);
-	percentFF[5] = createObject("100%F.png", oddBackground, 1137, 200, false);
+	percentFF[3] = createObject("source/picture/casino/10%F.png", oddBackground, 837, 200, false);
+	percentFF[4] = createObject("source/picture/casino/50%F.png", oddBackground, 982, 200, false);
+	percentFF[5] = createObject("source/picture/casino/100%F.png", oddBackground, 1137, 200, false);
 	for (int i = 3; i < 6; i++) {
 		scaleObject(percentFF[i], 0.5f);
 	}
